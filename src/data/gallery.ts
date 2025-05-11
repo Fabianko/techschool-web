2bba31e7c1bd53861625824b3e00e5bee279fc2f
Jaime Alan Gutiérrez Cruz <33694882+jagcruz@users.noreply.github.com>
2025-05-11 19:24:52 -0300

    feat: add image gallery component with slider functionality

---
+++ src/data/gallery.ts
@@ -0,0 +1,41 @@
+type GalleryItem = {
+  alt: string;
+  src: string;
+};
+
+export const gallery: GalleryItem[] = [
+  { alt: 'Image 1', src: '/src/images/gallery/01.webp' },
+  { alt: 'Image 2', src: '/src/images/gallery/02.webp' },
+  { alt: 'Image 3', src: '/src/images/gallery/03.webp' },
+  { alt: 'Image 4', src: '/src/images/gallery/04.webp' },
+  { alt: 'Image 5', src: '/src/images/gallery/05.webp' },
+  { alt: 'Image 6', src: '/src/images/gallery/06.webp' },
+  { alt: 'Image 7', src: '/src/images/gallery/07.webp' },
+  { alt: 'Image 8', src: '/src/images/gallery/08.webp' },
+  { alt: 'Image 9', src: '/src/images/gallery/09.webp' },
+  { alt: 'Image 10', src: '/src/images/gallery/10.webp' },
+  { alt: 'Image 11', src: '/src/images/gallery/11.webp' },
+  { alt: 'Image 12', src: '/src/images/gallery/12.webp' },
+  { alt: 'Image 13', src: '/src/images/gallery/13.webp' },
+  { alt: 'Image 14', src: '/src/images/gallery/14.webp' },
+  { alt: 'Image 15', src: '/src/images/gallery/15.webp' },
+  { alt: 'Image 16', src: '/src/images/gallery/16.webp' },
+  { alt: 'Image 17', src: '/src/images/gallery/17.webp' },
+  { alt: 'Image 18', src: '/src/images/gallery/18.webp' },
+  { alt: 'Image 19', src: '/src/images/gallery/19.webp' },
+  { alt: 'Image 20', src: '/src/images/gallery/20.webp' },
+  { alt: 'Image 21', src: '/src/images/gallery/21.webp' },
+  { alt: 'Image 22', src: '/src/images/gallery/22.webp' },
+  { alt: 'Image 23', src: '/src/images/gallery/23.webp' },
+  { alt: 'Image 24', src: '/src/images/gallery/24.webp' },
+  { alt: 'Image 25', src: '/src/images/gallery/25.webp' },
+  { alt: 'Image 26', src: '/src/images/gallery/26.webp' },
+  { alt: 'Image 27', src: '/src/images/gallery/27.webp' },
+  { alt: 'Image 28', src: '/src/images/gallery/28.webp' },
+  { alt: 'Image 29', src: '/src/images/gallery/29.webp' },
+  { alt: 'Image 30', src: '/src/images/gallery/30.webp' },
+  { alt: 'Image 31', src: '/src/images/gallery/31.webp' },
+  { alt: 'Image 32', src: '/src/images/gallery/32.webp' },
+  { alt: 'Image 33', src: '/src/images/gallery/33.webp' },
+  { alt: 'Image 34', src: '/src/images/gallery/34.webp' }
+];
