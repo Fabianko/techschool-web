582c959d09eb72632a1f7485d07b9afd974e8b50
Fabianko <fabian.arismendi@usach.cl>
2025-05-20 17:24:08 -0400

    feat: partners and rrss

---
+++ src/data/partners.ts
@@ -0,0 +1,18 @@
+type PertnerItem = {
+  name: string;
+  logo: string;
+  url: string;
+};
+
+export const partners: PertnerItem[] = [
+  {
+    name: 'DUOCUC',
+    logo: '/src/images/logos/duoc.png',
+    url: 'https://www.duoc.cl/escuela/informatica-telecomunicaciones/citt/'
+  },
+  {
+    name: 'CIIT DUOCUC',
+    logo: '/src/images/logos/ciit.png',
+    url: 'https://www.duoc.cl/escuela/informatica-telecomunicaciones/citt/'
+  }
+];
--- src/data/places.ts
+++ src/data/places.ts
@@ -5,24 +5,24 @@ export const places: TechPlace[] = [
     icon: '/src/images/states/santiago.webp',
     name: 'Santiago',
     direction: 'Av. Esq. Blanca 501, Maipú, Región Metropolitana, Chile',
-    link: '#'
+    link: 'https://lu.ma/8cxs0lvo'
   },
   {
     icon: '/src/images/states/valparaiso.webp',
     name: 'Valparaíso',
     direction: 'Brasil 2021, Valparaíso, Chile',
-    link: '#'
+    link: 'https://lu.ma/k4h60wdt'
   },
   {
     icon: '/src/images/states/puertomontt.webp',
     name: 'Puerto Montt',
     direction: 'Egaña 651, Puero Montt, Los Lagos, Chile',
-    link: '#'
+    link: 'https://lu.ma/vved6ciu'
   },
   {
     icon: '/src/images/states/concepcion.webp',
     name: 'Concepción',
     direction: 'Paicaví 3280, Concepción, Bío Bío, Chile',
-    link: '#'
+    link: 'https://lu.ma/b90s611e'
   }
 ];
